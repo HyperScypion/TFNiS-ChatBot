f9272ac4149e5b07bfe60cdee6491485582fd09b
hyperscypion <tomasz_derek@wp.pl>
2019-04-06 17:25:56 +0200

    code reformat

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template
+from flask import request, session, abort
 import os
- 
+
 app = Flask(__name__)
- 
+
+
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
         return "Hello Boss!"
- 
+
+
 @app.route('/login', methods=['POST'])
 def do_admin_login():
     if request.form['username'] != '':
@@ -20,6 +23,7 @@ def do_admin_login():
         flash('wrong password!')
         return home()
 
+
 @app.route('/logout')
 def logout():
     session['logged_in'] = False
@@ -28,7 +32,7 @@ def logout():
 # @app.route('/chat')
 # def chat():
 
- 
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4000)
+    app.run(debug=True, host='0.0.0.0', port=4000)
