7a0d69e30908bb1ffdf830df4d2fd167b2849c7c
hyperscypion <tomasz_derek@wp.pl>
2019-04-06 17:18:07 +0200

    added jaccard index

--- bot/botjacc.py
+++ bot/botjacc.py
@@ -8,6 +8,12 @@ class Bot:
     def jaccard(first_text, second_text):
 
         power = 0
-        return power
+        for i in range(int(len(first_text)) - 2):
+            for j in range(int(len(second_text)) - 2):
+                if first_text[i:i+3] == second_text[j:j+3]:
+                    power += 1
+
+        return (power/((int(len(first_text)) - 2) + (int(len(second_text)) - 2) - power))
+
 
 
