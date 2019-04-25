3451a1eacde0b1859885f479011ac6d28db28c91
hyperscypion <tomasz_derek@wp.pl>
2019-04-25 22:21:19 +0200

    Create corpus

--- bot/brain.py
+++ bot/brain.py
@@ -15,10 +15,20 @@ class Brain:
         self.data_frame = pd.read_csv(path, header=None)
         return self.data_frame
     
-    def create_corpus(self):
-        pass
+    def create_corpus(self, save=True):
+        self.text_to_corp = []
+        for i in range(len(self.data_frame)):
+            self.text_to_corp.append(self.data_frame[0][i].split())
+            self.dictionary = corpora.Dictionary(self.text_to_corp)
+            if save == True:
+                self.dictionary.save('/tmp/deerwester.dict')
+            self.corpus = [self.dictionary.doc2vow(text) 
+                    for text in self.text_to_corp]
+            corpora.MmCorpus.serialize('/tmp/deerwester.mm', corpus)
+            self.id2word = corpora.Dictionary.load('/tmp/deerwester.dict')
+            return self.corpus
 
-    def predict(self, text):
+    def predict(self, text, jaccard=False):
         pass
 
 
