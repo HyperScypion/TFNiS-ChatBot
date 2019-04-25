3a627bb40e7c2a7932c7a7cce0b7acbfe5b7cdfd
hyperscypion <tomasz_derek@wp.pl>
2019-04-25 22:46:30 +0200

    Code reformat and fixed bugs

--- bot/brain.py
+++ bot/brain.py
@@ -11,24 +11,26 @@ class Brain:
     def __init__(self):
         pass
 
+    #   @staticmethod
     def load_model(self, path='/home/hyperscypion/Desktop/database.csv'):
         self.data_frame = pd.read_csv(path, header=None)
         return self.data_frame
-    
+
     def create_corpus(self, save=True):
         self.text_to_corp = []
         for i in range(len(self.data_frame)):
             self.text_to_corp.append(self.data_frame[0][i].split())
-            self.dictionary = corpora.Dictionary(self.text_to_corp)
-            if save == True:
-                self.dictionary.save('/tmp/deerwester.dict')
-            self.corpus = [self.dictionary.doc2vow(text) 
-                    for text in self.text_to_corp]
-            corpora.MmCorpus.serialize('/tmp/deerwester.mm', corpus)
-            return self.corpus
+
+        self.dictionary = corpora.Dictionary(self.text_to_corp)
+        if save == True:
+            self.dictionary.save('/tmp/deerwester.dict')
+        self.corpus = [self.dictionary.doc2bow(text)
+                       for text in self.text_to_corp]
+        corpora.MmCorpus.serialize('/tmp/deerwester.mm', self.corpus)
+        return self.corpus
+
+    def fit_LDA(self, corpus, num_topics=10):
+        self.lda = LdaModel(corpus, num_topics)
 
     def predict(self, text, jaccard=False):
         pass
-
-
-
