0be5a834e1ed6194a7bd7270743c907c23f38a04
hyperscypion <tomasz_derek@wp.pl>
2019-04-25 23:46:08 +0200

    Added jaccard index

--- bot/brain.py
+++ bot/brain.py
@@ -11,7 +11,6 @@ class Brain:
     def __init__(self):
         pass
 
-    #   @staticmethod
     def load_model(self, path='/home/hyperscypion/Desktop/database.csv'):
         self.data_frame = pd.read_csv(path, header=None)
         return self.data_frame
@@ -27,10 +26,23 @@ class Brain:
         self.corpus = [self.dictionary.doc2bow(text)
                        for text in self.text_to_corp]
         corpora.MmCorpus.serialize('/tmp/deerwester.mm', self.corpus)
+        self.id2word = corpora.Dictionary.load('/tmp/deerwester.dict')
         return self.corpus
 
     def fit_LDA(self, corpus, num_topics=10):
-        self.lda = LdaModel(corpus, num_topics)
+        self.lda = LdaModel(corpus, num_topics, id2word=self.id2word)
 
     def predict(self, text, jaccard=False):
-        pass
+        jacc = Jaccard()
+        prediction = -1
+        max_index = 0
+
+        if jaccard == True:
+            for i in range(len(self.data_frame)):
+                if jacc._compute_index(text, str(self.data_frame[0][i])) > prediction:
+                    prediction = jacc._compute_index(text, str(self.data_frame[0][i]))
+                    max_index = i
+            return self.data_frame[1][max_index]
+
+        else:
+            pass
