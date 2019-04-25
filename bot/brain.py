b6455193b631823ed8d37a1be454327f306790a1
hyperscypion <tomasz_derek@wp.pl>
2019-04-26 01:24:48 +0200

    Added LDA and Jaccard index

--- bot/brain.py
+++ bot/brain.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import pandas as pd
 from jaccard import Jaccard
@@ -11,7 +12,7 @@ class Brain:
     def __init__(self):
         pass
 
-    def load_model(self, path='/home/hyperscypion/Desktop/database.csv'):
+    def load_model(self, path='/home/hyperscypion/Desktop/database_lda.csv'):
         self.data_frame = pd.read_csv(path, header=None)
         return self.data_frame
 
@@ -27,22 +28,39 @@ class Brain:
                        for text in self.text_to_corp]
         corpora.MmCorpus.serialize('/tmp/deerwester.mm', self.corpus)
         self.id2word = corpora.Dictionary.load('/tmp/deerwester.dict')
-        return self.corpus
+        return self.corpus, self.dictionary
 
     def fit_LDA(self, corpus, num_topics=10):
         self.lda = LdaModel(corpus, num_topics, id2word=self.id2word)
+        return self.lda
 
-    def predict(self, text, jaccard=False):
+    def predict(self, text, dictionary, jaccard=False):
         jacc = Jaccard()
         prediction = -1
-        max_index = 0
+        list_of_indexes = []
+        list_of_topics = []
 
         if jaccard == True:
             for i in range(len(self.data_frame)):
                 if jacc._compute_index(text, str(self.data_frame[0][i])) > prediction:
                     prediction = jacc._compute_index(text, str(self.data_frame[0][i]))
-                    max_index = i
-            return self.data_frame[1][max_index]
 
+            for i in range(len(self.data_frame)):
+                if jacc._compute_index(text, str(self.data_frame[0][i])) == prediction:
+                    list_of_indexes.append(i)
+            max_random_index = random.randint(0, len(list_of_indexes) - 1)
+            return self.data_frame[1][list_of_indexes[max_random_index]]
         else:
-            pass
+            bow = dictionary.doc2bow(text.split())
+            for index, score in sorted(self.lda[bow], key=lambda tup: -1 * tup[1]):
+                break
+            for i in range(len(self.data_frame)):
+                if index == self.data_frame[2][i]:
+                    list_of_topics.append(self.data_frame[0][i])
+
+            for i in list_of_topics:
+                if jacc._compute_index(text, i) > prediction:
+                    prediction = jacc._compute_index(text, i)
+
+            max_random_ans = random.randint(0, len(list_of_topics) - 1)
+            return list_of_topics[max_random_ans]
