6195edc68368f2342f41aa14f5ac40d9d4ef5397
hyperscypion <tomasz_derek@wp.pl>
2019-04-27 02:27:02 +0200

    Major fixes to frontend

--- main.py
+++ main.py
@@ -18,45 +18,14 @@ brain.fit_LDA(corpus, 40)
 @app.route('/')
 def home():
     if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return chat()
+        return render_template('index.html')
 
-
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    if request.form['username'] != '':
-        session['logged_in'] = True
-        query = "SELECT * FROM users;"
-        engine = create_engine('sqlite:///bot.db', echo=True)
-        connection = engine.connect()
-        result = connection.execute(query)
-        print(result)
-        return chat()
-    else:
-        return home()
-
-
-@app.route('/logout')
-def logout():
-    session['logged_in'] = False
-    return home()
-
-app.route('/chat')
+@app.route('/', methods=['POST'])
 def chat():     
-    return render_template('chat.html')
-
-
-
-@app.route('/process',methods=['POST'])
-def process():
-	user_input=request.form['user_input']
-
-	bot_response=brain.predict(str(user_input), dictionary, False)
-	print("ChatBot: " + bot_response)
-	return render_template('chat.html', user_input=user_input,
-		bot_response=bot_response
-)
+    user_input = request.form.get('textbox')
+    bot_response=brain.predict(str(user_input), dictionary, False)
+    print("ChatBot: " + bot_response)
+    return render_template('index.html', textbox=user_input, bot_response=bot_response)
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
